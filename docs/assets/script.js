80e4029a7539197a0b01f3d888769175a6d19c52
Alexander <alex.m.sky@gmail.com>
2019-02-18 11:56:36 +0300

    *

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -199,5 +199,42 @@ window.onresize = () => drawLine();
   scroll.onclick = TopscrollTo;
 
 })();
-
 // /Scroll to top
+
+// активный пункт меню
+(() => {
+  var activeNavMenu = () =>
+  {
+    var currentUrl = window.location.pathname.split('/'),
+      menuA = document.querySelectorAll('.navbar__navs a'),
+      menuAlength = menuA.length;
+    for (var i = 0; i < menuAlength; i++) {
+      for (var q = 0; q < currentUrl.length; q++) {
+        if (currentUrl[q] === menuA[i].getAttribute('href')) {
+          menuA[i].classList.add('active');
+          break;
+        }
+      }
+    }
+
+  };
+  activeNavMenu();
+
+  var activeMenu = () =>
+  {
+    var currentUrl = window.location.pathname.split('/'),
+      menuA = document.querySelectorAll('.footer__nav-items a'),
+      menuAlength = menuA.length;
+    for (var i = 0; i < menuAlength; i++) {
+      for (var q = 0; q < currentUrl.length; q++) {
+        if (currentUrl[q] === menuA[i].getAttribute('href')) {
+          menuA[i].classList.add('active');
+          break;
+        }
+      }
+    }
+
+  };
+  activeMenu();
+})();
+// /активный пункт меню
